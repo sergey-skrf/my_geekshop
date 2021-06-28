1fbb7e393a77903136f57ca43a8b1f332b41eedb
Sergey-skrf <sergey_skrf@mail.ru>
2021-06-28 22:24:44 +0700

    вывод динамического контента на странице products (список товаров)

--- products/views.py
+++ products/views.py
@@ -11,7 +11,27 @@ def index(request):
 
 def products(request):
     context = {
-        'title': 'GeekShop - Каталог'
+        'title': 'GeekShop - Каталог',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals',
+             'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
+             'price': 6090},
+            {'name': 'Синяя куртка The North Face',
+             'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
+             'price': 23725},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
+             'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
+             'price': 3390},
+            {'name': 'Черный рюкзак Nike Heritage',
+             'description': 'Плотная ткань. Легкий материал.',
+             'price': 2340},
+            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
+             'description': 'Гладкий кожаный верх. Натуральный материал.',
+             'price': 13590},
+            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN',
+             'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.',
+             'price': 2890},
+        ],
     }
     return render(request, 'products/products.html', context)
 
