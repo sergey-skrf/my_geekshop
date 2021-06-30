3df6c94aed3139a5283cf1af812f4ff858cfce8f
Sergey-skrf <sergey_skrf@mail.ru>
2021-07-01 00:34:01 +0700

    организована работа с моделями в контроллерах и шаблонах

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from products.views import index, products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', products, name='products')
+    path('products/', include('products.urls', namespace='products'))
 ]
