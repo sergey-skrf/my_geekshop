1ee34f51a756c0b410e106bebaef2de10c2337a3
Sergey-skrf <sergey_skrf@mail.ru>
2021-07-02 00:05:04 +0700

    организовал работу с моделями в контроллерах и шаблонах

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-#from django.conf import settings - с таким импортом не работает
-from geekshop import settings
+from django.conf import settings #- с таким импортом не работает
+#from geekshop import settings
 from django.conf.urls.static import static
 
 from products.views import index
@@ -28,4 +28,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URl, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ import os
 import json
 
 from django.shortcuts import render
+from products.models import ProductCategory, Product
 
 MODULE_DIR = os.path.dirname(__file__)
 
@@ -15,7 +16,10 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 def products(request):
-    context = {'title': 'GeekShop - Каталог'}
-    file_path = os.path.join(MODULE_DIR, "fixtures/goods.json")
-    context['products'] = json.load(open(file_path, encoding='utf-8'))
+    context = {'title': 'GeekShop - Каталог',
+               'products': Product.objects.all(),
+               'category': ProductCategory.objects.all()
+               }
+    #file_path = os.path.join(MODULE_DIR, "fixtures/goods.json")
+    #context['products'] = json.load(open(file_path, encoding='utf-8'))
     return render(request, 'products/products.html', context)
