4b9ae288fcf290a8429a18169dbab908257a6f06
Sergey-skrf <sergey_skrf@mail.ru>
2021-07-03 11:17:40 +0700

    добавил AUTH_USER_MODEL = 'users.User'

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products'
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +130,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#AUTH_USER_MODEL = 'users.User'
