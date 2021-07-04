bc725a47962e4ec4932ee8b6df80a6973bba844e
Sergey-skrf <sergey_skrf@mail.ru>
2021-07-04 11:20:22 +0700

    добавил logout

--- users/views.py
+++ users/views.py
@@ -33,3 +33,8 @@ def registration(request):
         form = UserRegistrationForm()
     context = {'title': 'GeekShop - Регистрация', 'form': form}
     return render(request, 'users/register.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
