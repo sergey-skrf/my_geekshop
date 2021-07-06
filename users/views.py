79b813c881c3fb244d5136a8dabaa42731da0760
Sergey-skrf <sergey_skrf@mail.ru>
2021-07-06 22:02:07 +0700

    Добавил вывод сообщения об успешних действиях при регистрации

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 
 from users.forms import UserLoginForm, UserRegistrationForm
@@ -26,6 +26,7 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Вы успешно зарегестрировались!')
             return HttpResponseRedirect(reverse('users:login'))
     else:
         form = UserRegistrationForm()
