1d3f8cd66e32f4619ff3e1a8051d0f2b8766adf0
Sergey-skrf <sergey_skrf@mail.ru>
2021-07-06 21:24:12 +0700

    Добавил обработку ошибок для страницы авторизации

--- users/views.py
+++ users/views.py
@@ -14,8 +14,6 @@ def login(request):
             if user and user.is_active:
                 auth.login(request, user)
                 return HttpResponseRedirect(reverse('index'))
-        else:
-            print(form.errors)
     else:
         form = UserLoginForm()
     context = {
