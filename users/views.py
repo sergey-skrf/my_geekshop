61be7c5ce26755328ed534936f313481df4d9aa4
Sergey-skrf <sergey_skrf@mail.ru>
2021-07-06 23:47:12 +0700

    added UserProfileForm

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 def login(request):
     if request.method == 'POST':
@@ -34,7 +34,8 @@ def registration(request):
     return render(request, 'users/register.html', context)
 
 def profile(request):
-    context = {'title': 'GeekShop - Личный кабинет'}
+    form = UserProfileForm(instance=request.user)
+    context = {'title': 'GeekShop - Личный кабинет', 'form': form}
     return render(request, 'users/profile.html', context)
 
 
