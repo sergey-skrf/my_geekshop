78caaaaf87c7029a36fe60610958b320c8364901
Sergey-skrf <sergey_skrf@mail.ru>
2021-07-15 23:05:36 +0700

    добавление пользователя в админке

---
+++ admins/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from users.models import User
+
+from users.forms import UserRegistrationForm
+
+class UserAdminRegistrationForm(UserRegistrationForm):
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}), required=False)
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'image', 'first_name', 'last_name', 'password1', 'password2')
--- admins/views.py
+++ admins/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from users.models import User
+from admins.forms import UserAdminRegistrationForm
+from django.urls import reverse
 
 def index(request):
     return render(request, 'admins/index.html')
@@ -9,7 +11,19 @@ def admin_users(request):
     return render(request, 'admins/admin-users-read.html', context)
 
 def admin_users_create(request):
-    return render(request, 'admins/admin-users-create.html')
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+        else:
+            print(form.errors)
+    else:
+        form = UserAdminRegistrationForm()
+    context = {
+        'title': 'Админ-панель - Создание пользователя', 'form': form}
+    return render(request, 'admins/admin-users-create.html', context)
+
 
 def admin_users_update_delete(request):
     return render(request, 'admins/admin-users-update-delete.html')
