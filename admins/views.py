4fc9e5a4a9b47a5d2b07f24255369923885b621c
Sergey-skrf <sergey_skrf@mail.ru>
2021-07-15 23:15:28 +0700

    добавил context в контроллеры index и admin_users, а в context добавил title

--- admins/views.py
+++ admins/views.py
@@ -4,10 +4,11 @@ from admins.forms import UserAdminRegistrationForm
 from django.urls import reverse
 
 def index(request):
-    return render(request, 'admins/index.html')
+    context = {'title': 'Админ-панель'}
+    return render(request, 'admins/index.html', context)
 
 def admin_users(request):
-    context = {'users': User.objects.all()}
+    context = {'title': 'Админ-панель - Пользователи', 'users': User.objects.all()}
     return render(request, 'admins/admin-users-read.html', context)
 
 def admin_users_create(request):
