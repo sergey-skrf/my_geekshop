2981824153176abce825b6341225a52abd356b88
Sergey-skrf <sergey_skrf@mail.ru>
2021-07-16 01:18:36 +0700

    преобразовал админку Django

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 from products.models import ProductCategory, Product
 
-admin.site.register(Product)
+#admin.site.register(Product)
 admin.site.register(ProductCategory)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'image', 'description', 'category', ('price', 'quantity'))
+    readonly_fields = ('description',)
+    ordering = ('quantity',)
+    search_fields = ('name',)
