fc5dfd40288e74236742ef0750569e780e5709fb
Sergey-skrf <sergey_skrf@mail.ru>
2021-07-16 00:09:08 +0700

    добавил обновление пользователя в админке

--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from users.models import User
-from admins.forms import UserAdminRegistrationForm
+from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 from django.urls import reverse
 
 def index(request):
@@ -27,7 +27,22 @@ def admin_users_create(request):
 
 
 def admin_users_update(request, pk):
-    return render(request, 'admins/admin-users-update-delete.html')
+    selected_user = User.objects.get(id=pk)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(instance=selected_user,files=request.FILES, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+        else:
+            print(form.errors)
+    else:
+        form = UserAdminProfileForm(instance=selected_user)
+    context = {
+        'title': 'Админ-панель - Редактирование пользователя',
+        'form': form,
+        'selected_user': selected_user,
+    }
+    return render(request, 'admins/admin-users-update-delete.html', context)
 
 def admin_users_remove(request, pk):
     pass
