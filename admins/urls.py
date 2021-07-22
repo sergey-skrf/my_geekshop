a8ae3f97cd246db47b0cbe0393f07b3b0deeddf1
Sergey-skrf <sergey_skrf@mail.ru>
2021-07-22 23:37:52 +0700

    CVB - UserDeleteView

--- admins/urls.py
+++ admins/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from admins.views import index, UserListView, UserCreateView, UserUpdateView, \
-    admin_users_remove, admin_products, admin_categories
+    UserDeleteView, admin_products, admin_categories
 
 app_name = 'admins'
 urlpatterns = [
@@ -8,7 +8,7 @@ urlpatterns = [
     path('users/', UserListView.as_view(), name='admin_users'),
     path('users/create/', UserCreateView.as_view(), name='admin_users_create'),
     path('users/update/<int:pk>/', UserUpdateView.as_view(), name='admin_users_update'),
-    path('users/remove/<int:pk>/', admin_users_remove, name='admin_users_remove'),
+    path('users/remove/<int:pk>/', UserDeleteView.as_view(), name='admin_users_remove'),
     path('products/', admin_products, name='admin_products'),
     path('categories/', admin_categories, name='admin_categories'),
 
--- admins/views.py
+++ admins/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from users.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
@@ -31,13 +31,12 @@ class UserUpdateView(UpdateView):
     success_url = reverse_lazy('admins:admin_users')
 
 
+class UserDeleteView(DeleteView):
+    model = User
+    template_name = 'admins/admin-users-update-delete.html'
+    success_url = reverse_lazy('admins:admin_users')
 
-@user_passes_test(lambda u: u.is_staff)
-def admin_users_remove(request, pk):
-    user = User.objects.get(id=pk)
-    user.is_active = False
-    user.save()
-    return HttpResponseRedirect(reverse('admins:admin_users'))
+
 
 #Контроллеры для товаров:
 @user_passes_test(lambda u: u.is_staff)
@@ -93,3 +92,11 @@ def admin_categories(request):
 #        'selected_user': selected_user,
 #    }
 #    return render(request, 'admins/admin-users-update-delete.html', context)
+
+
+#@user_passes_test(lambda u: u.is_staff)
+#def admin_users_remove(request, pk):
+#    user = User.objects.get(id=pk)
+#    user.is_active = False
+#    user.save()
+#    return HttpResponseRedirect(reverse('admins:admin_users'))
